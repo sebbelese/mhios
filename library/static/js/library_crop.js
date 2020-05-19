f8773bf77b40ce919a36b6740a16917e9a4c40ed
sebbelese <sebastienblaise@laposte.net>
2020-05-20 00:19:22 +0200

    Using new cropper.js. Crop not at the right coordinates: to check. Also: check if we need to update python for each event. SHould not be an issue however

--- library/static/js/library_crop.js
+++ library/static/js/library_crop.js
@@ -1,18 +1,32 @@
 $(function () {
     
-    /* SCRIPT TO OPEN THE MODAL WITH THE PREVIEW */
+    /* SCRIPT TO SHOW THE PICTURE WHEN UPLOADED */
     $("#id_poster").change(function () {
         if (this.files && this.files[0]) {
             var reader = new FileReader();
+	    var file;
             reader.onload = function (e) {
-		$("#image").attr("src", e.target.result);
-		$("#modalCrop").modal("show");
+		$("#image").attr("src",e.target.result);
+		const image = document.getElementById('image');
+		const cropper = new Cropper(image, {
+		    aspectRatio: 1 / 1,
+		    crop(event) {
+			$("#id_x").val(event.detail.x);
+			$("#id_y").val(event.detail.y);
+			$("#id_height").val(event.detail.height);
+			$("#id_width").val(event.detail.width);
+		    },
+		});
             }
-            reader.readAsDataURL(this.files[0]);
+	    reader.readAsDataURL(this.files[0]);
         }
+	
+
     });
-    
-    /* SCRIPTS TO HANDLE THE CROPPER BOX */
+  
+});
+  
+    /* SCRIPTS TO HANDLE THE CROPPER BOX 
     var $image = $("#image");
     var cropBoxData;
     var canvasData;
@@ -31,14 +45,13 @@ $(function () {
         $image.cropper("destroy");
     });
     
-    /* SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER */
+    SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER 
     $(".js-crop").click(function () {
         var cropData = $image.cropper("getData");
         $("#id_x").val(cropData["x"]);
         $("#id_y").val(cropData["y"]);
         $("#id_height").val(cropData["height"]);
         $("#id_width").val(cropData["width"]);
-/*        $("#formUpload").submit();*/
-    });
+
+    });*/
     
-});
