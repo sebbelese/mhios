f431718791d00d6031d4025388f330dc576d7652
sebbelese <sebastienblaise@laposte.net>
2020-05-24 21:28:45 +0200

    static serve files

--- library/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from image_cropping import ImageCroppingMixin
-from .models import story
-
-class storyAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    pass
-
-admin.site.register(story, storyAdmin)
