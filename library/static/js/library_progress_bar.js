6229e8c7a64e60cbd2553a7dde0e66de32a902ba
sebbelese <sebastienblaise@laposte.net>
2020-06-02 22:56:06 +0200

    progress bar for form upload

---
+++ library/static/js/library_progress_bar.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#uploadButton").click(function() {
+        var formData = new FormData($('#formUpload')[0]);
+        console.log(formData);
+        $.ajax({
+            url: "#",
+            type: 'POST',
+            data: formData,
+            xhr: function() {
+                var xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function(evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = evt.loaded / evt.total;
+			console.log(evt.loaded);
+			console.log(evt.total);
+                        console.log(percentComplete);
+                        $('#status').html('<b> Uploading -> ' + (Math.round(percentComplete * 100)) + '% </b>');
+                    }
+                }, false);
+                return xhr;
+            },
+            success: function(data) {
+                $("#status").html('UPLOADED!!');
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+        return false;
+    });
+});
